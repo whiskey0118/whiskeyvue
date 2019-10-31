26bc7f7a15e7cc97f8ff6675e2503768b00bc376
whiskey <whiskey0118@sina.com>
2019-10-31 18:56:51 +0800

    element 测试

--- src/components/test/Test2.vue
+++ src/components/test/Test2.vue
@@ -1,18 +1,9 @@
 <template>
-    <el-form :rules="rules" class="login-container" label-position="left"
-             label-width="0px" v-loading="loading">
-        <h3 class="login_title">系统登录</h3>
-        <el-form-item prop="account">
-            <span>hahaha</span>
-        </el-form-item>
-        <el-form-item prop="checkPass">
-            <span>hahaha</span>
-        </el-form-item>
-            <el-checkbox class="login_remember" v-model="checked" label-position="left">记住密码</el-checkbox>
-        <el-form-item style="width: 100%">
-            <el-button type="primary">登录</el-button>
-        </el-form-item>
-    </el-form>
+    <el-row>
+        <el-col :span="24"><div class="grid-content bg-purple-dark">hahaha直营未来</div></el-col>
+    </el-row>
+
+
 </template>
 <script>
     import test from './test'
@@ -25,25 +16,7 @@
 </script>
 
 <style>
-    .login-container {
-        border-radius: 15px;
-        background-clip: padding-box;
-        margin: 180px auto;
-        width: 350px;
-        padding: 35px 35px 15px 35px;
-        background: #fff;
-        border: 1px solid #eaeaea;
-        box-shadow: 0 0 25px #cac6c6;
-    }
+    .grid-content{
 
-    .login_title {
-        margin: 0px auto 40px auto;
-        text-align: center;
-        color: #505458;
-    }
-
-    .login_remember {
-        margin: 0px 0px 35px 0px;
-        text-align: left;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import VueRouter from "vue-router"
 import ElementUI from 'element-ui'
 import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(ElementUI)
+
 
 
 new Vue({
